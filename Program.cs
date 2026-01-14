214d1dc3c2e959b5528d002f2ab7bcc5606dcb6c
Eduardo Barroso <edbar42@proton.me>
2026-01-14 19:11:53 -0300

    feat: Create and read employees list

--- Program.cs
+++ Program.cs
@@ -1,20 +1,69 @@
-var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+var employeeRoute = app.MapGroup("employees");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+var employees = new List<Employee>
+{
+    new Employee
+    {
+        Id = 1,
+        FirstName = "John",
+        LastName = "Nightreign",
+    },
+    new Employee
+    {
+        Id = 2,
+        FirstName = "Jack",
+        LastName = "Harlow",
+    },
+};
 
 app.UseHttpsRedirection();
 
-app.MapGet("/employees")
+employeeRoute.MapGet(
+    string.Empty,
+    () =>
+    {
+        return Results.Ok(employees);
+    }
+);
+
+employeeRoute.MapGet(
+    "/{id:int}",
+    (int id) =>
+    {
+        var employee = employees.SingleOrDefault(e => e.Id == id);
+        if (employee == null)
+        {
+            return Results.NotFound();
+        }
+
+        return Results.Ok(employee);
+    }
+);
+
+employeeRoute.MapPost(
+    string.Empty,
+    (Employee employee) =>
+    {
+        employee.Id = employees.Max(e => e.Id) + 1;
+        employees.Add(employee);
+        return Results.Created($"/employees/{employee.Id}", employee);
+    }
+);
 
 app.Run();
-
