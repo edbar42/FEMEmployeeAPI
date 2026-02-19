cfcf3ab65428e41e0d83113b564797a4aebc88f1
Eduardo Barroso <edbar42@proton.me>
2026-02-19 16:29:25 -0300

    chore: refactor to controllers

--- FEMEmployeeAPI/Extensions.cs
+++ FEMEmployeeAPI/Extensions.cs
@@ -1,35 +1,51 @@
 using System.ComponentModel.DataAnnotations;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace FEMEmployeeAPI
 {
-    public static class Extensions
+  public static class Extensions
+  {
+
+    public static ModelStateDictionary ToModelStateDictionary(this FluentValidation.Results.ValidationResult validationResult)
     {
-        public static ValidationProblemDetails ToValidationProblemDetails(
-            this List<ValidationResult> validationResults
-        )
-        {
-            var problemDetails = new ValidationProblemDetails();
+      var modelState = new ModelStateDictionary();
 
-            foreach (var validationResult in validationResults)
-            {
-                foreach (var memberName in validationResult.MemberNames)
-                {
-                    if (problemDetails.Errors.ContainsKey(memberName))
-                    {
-                        problemDetails.Errors[memberName] = problemDetails
-                            .Errors[memberName]
-                            .Concat([validationResult.ErrorMessage])
-                            .ToArray()!;
-                    }
-                    else
-                    {
-                        problemDetails.Errors[memberName] = [validationResult.ErrorMessage!];
-                    }
-                }
-            }
+      foreach (var error in validationResult.Errors)
+      {
+        modelState.AddModelError(error.PropertyName, error.ErrorMessage);
+      }
 
-            return problemDetails;
-        }
+      return modelState;
+
     }
+
+    public static ValidationProblemDetails ToValidationProblemDetails(
+        this List<System.ComponentModel.DataAnnotations.ValidationResult> validationResults
+    )
+    {
+      var problemDetails = new ValidationProblemDetails();
+
+      foreach (var validationResult in validationResults)
+      {
+        foreach (var memberName in validationResult.MemberNames)
+        {
+          if (problemDetails.Errors.ContainsKey(memberName))
+          {
+            problemDetails.Errors[memberName] = problemDetails
+                .Errors[memberName]
+                .Concat([validationResult.ErrorMessage])
+                .ToArray()!;
+          }
+          else
+          {
+            problemDetails.Errors[memberName] = [validationResult.ErrorMessage!];
+          }
+        }
+      }
+
+      return problemDetails;
+    }
+  }
 }
--- FEMEmployeeAPI/Program.cs
+++ FEMEmployeeAPI/Program.cs
@@ -7,9 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<EmployeeRepository>();
+builder.Services.AddSingleton<IRepository<Employee>, EmployeeRepository>();
 builder.Services.AddProblemDetails();
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -18,105 +19,12 @@ var employeeRoute = app.MapGroup("employees");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-employeeRoute.MapGet(
-    string.Empty,
-    (EmployeeRepository repo) =>
-    {
-        return Results.Ok(
-            repo.GetAll()
-                .Select(employee => new GetEmployeeResponse
-                {
-                    FirstName = employee.FirstName,
-                    LastName = employee.LastName,
-                    Address1 = employee.Address1,
-                    Address2 = employee.Address2,
-                    City = employee.City,
-                    State = employee.State,
-                    ZipCode = employee.ZipCode,
-                    PhoneNumber = employee.PhoneNumber,
-                    Email = employee.Email,
-                })
-        );
-    }
-);
-
-employeeRoute.MapGet(
-    "/{id:int}",
-    (int id, EmployeeRepository repo) =>
-    {
-        var employee = repo.GetById(id);
-        if (employee == null)
-        {
-            return Results.NotFound();
-        }
-
-        return Results.Ok(
-            new GetEmployeeResponse
-            {
-                FirstName = employee.FirstName,
-                LastName = employee.LastName,
-                Address1 = employee.Address1,
-                Address2 = employee.Address2,
-                City = employee.City,
-                State = employee.State,
-                ZipCode = employee.ZipCode,
-                PhoneNumber = employee.PhoneNumber,
-                Email = employee.Email,
-            }
-        );
-    }
-);
-
-employeeRoute.MapPost(string.Empty, async (CreateEmployeeRequest employeeRequest, IRepository<Employee> repository, IValidator<CreateEmployeeRequest> validator) => {
-    var validationResults = await validator.ValidateAsync(employeeRequest);
-    if (!validationResults.IsValid)
-    {
-        return Results.ValidationProblem(validationResults.ToDictionary());
-    }
-
-    var newEmployee = new Employee {
-        FirstName = employeeRequest.FirstName!,
-        LastName = employeeRequest.LastName!,
-        SocialSecurityNumber = employeeRequest.SocialSecurityNumber!,
-        Address1 = employeeRequest.Address1,
-        Address2 = employeeRequest.Address2,
-        City = employeeRequest.City,
-        State = employeeRequest.State,
-        ZipCode = employeeRequest.ZipCode,
-        PhoneNumber = employeeRequest.PhoneNumber,
-        Email = employeeRequest.Email
-    };
-    repository.Create(newEmployee);
-    return Results.Created($"/employees/{newEmployee.Id}", employeeRequest);
-});
-
-employeeRoute.MapPut(
-    "{id}",
-    (UpdateEmployeeRequest employee, int id, EmployeeRepository repo) =>
-    {
-        var existingEmployee = repo.GetById(id);
-        if (existingEmployee == null)
-        {
-            return Results.NotFound();
-        }
-
-        existingEmployee.Address1 = employee.Address1;
-        existingEmployee.Address2 = employee.Address2;
-        existingEmployee.City = employee.City;
-        existingEmployee.State = employee.State;
-        existingEmployee.ZipCode = employee.ZipCode;
-        existingEmployee.PhoneNumber = employee.PhoneNumber;
-        existingEmployee.Email = employee.Email;
-
-        repo.Update(existingEmployee);
-        return Results.Ok(existingEmployee);
-    }
-);
+app.MapControllers();
 
 app.Run();
